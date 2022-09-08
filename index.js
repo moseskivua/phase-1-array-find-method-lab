74c3b36548caa76ae4714faabf8270c456f49cb5
Moses Kivua <kivuamoses9@gmail.com>
2022-09-08 12:39:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+const chai = require('chai')
+global.expect = chai.expect
+const fs = require('file-system')
+const fs= require('fs')
+const jsdom = require('mocha-jsdom')
+const path = require('path')
+const babel = require('babel-core');
